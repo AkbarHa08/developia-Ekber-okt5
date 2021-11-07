543e701c0a5df078911bb8af1466f251de640b6e
AkbarHa08 <68279197+AkbarHa08@users.noreply.github.com>
2021-11-07 13:59:17 +0300

    Rest Delete method

--- projects/spring-oktyabr5-project/src/main/java/az/developia/springoktyabr5project/controller/BookRestController.java
+++ projects/spring-oktyabr5-project/src/main/java/az/developia/springoktyabr5project/controller/BookRestController.java
@@ -3,6 +3,8 @@ package az.developia.springoktyabr5project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +27,10 @@ public class BookRestController {
       bookRepository.save(book);
 
     }
+	
+	@DeleteMapping(path="/{id}")
+	public void delete(@PathVariable Integer id) {
+		bookRepository.deleteById(id);
+	}
 	
 }
