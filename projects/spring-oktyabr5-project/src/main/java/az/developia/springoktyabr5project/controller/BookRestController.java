e5c5e858c5e9b30797442d032fe0f8907364451c
AkbarHa08 <68279197+AkbarHa08@users.noreply.github.com>
2021-11-07 13:18:47 +0300

    Rest Post method

---
+++ projects/spring-oktyabr5-project/src/main/java/az/developia/springoktyabr5project/controller/BookRestController.java
@@ -0,0 +1,29 @@
+package az.developia.springoktyabr5project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import az.developia.springoktyabr5project.model.Book;
+import az.developia.springoktyabr5project.repository.BookRepository;
+
+@Controller
+@RequestMapping(path = "/books/rest")
+@CrossOrigin(origins = "*")
+public class BookRestController {
+	
+	 @Autowired
+	    private BookRepository bookRepository;
+	
+
+	@PostMapping
+    public void addBook(@RequestBody Book book) {
+      bookRepository.save(book);
+
+    }
+	
+}
