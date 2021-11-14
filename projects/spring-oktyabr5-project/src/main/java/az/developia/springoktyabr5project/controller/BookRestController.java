e30913fd19f367a89cbd654b5ace79c148a29f53
AkbarHa08 <68279197+AkbarHa08@users.noreply.github.com>
2021-11-14 14:11:59 +0300

    spring select data from checkbox with rest

--- projects/spring-oktyabr5-project/src/main/java/az/developia/springoktyabr5project/controller/BookRestController.java
+++ projects/spring-oktyabr5-project/src/main/java/az/developia/springoktyabr5project/controller/BookRestController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,6 +32,11 @@ public class BookRestController {
 	@DeleteMapping(path="/{id}")
 	public void delete(@PathVariable Integer id) {
 		bookRepository.deleteById(id);
+	}
+	
+	@GetMapping(path="/{id}")
+	public Book getById(@PathVariable Integer id) {
+		return bookRepository.findById(id).get();
 	}
 	
 }
