3bc4fcd63f2b65e7676edd5335d3b218c22aec7a
AkbarHa08 <68279197+AkbarHa08@users.noreply.github.com>
2021-12-08 16:35:27 +0300

    Revert "in memory database"
    This reverts commit 37d278246978a78a4d7834c78acede821562c604.

--- projects/spring-oktyabr5-project/src/main/java/az/developia/springoktyabr5project/config/SecurityConfig.java
+++ projects/spring-oktyabr5-project/src/main/java/az/developia/springoktyabr5project/config/SecurityConfig.java
@@ -1,19 +1,17 @@
-//package az.developia.springoktyabr5project.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@EnableWebSecurity
-//@Configuration
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-//		.antMatchers("/h2-console/**").permitAll()
-//		.anyRequest().authenticated().and().httpBasic();
-//	}
-//}
+package az.developia.springoktyabr5project.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest().authenticated().and().httpBasic();
+	}
+}
