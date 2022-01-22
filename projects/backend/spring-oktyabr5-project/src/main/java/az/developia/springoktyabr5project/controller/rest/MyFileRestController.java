d7f09330e01fa3395ac129f09c797d910a89e5dd
AkbarHa08 <68279197+AkbarHa08@users.noreply.github.com>
2022-01-22 11:02:22 +0300

    file upload kodu

--- projects/backend/spring-oktyabr5-project/src/main/java/az/developia/springoktyabr5project/controller/rest/MyFileRestController.java
+++ projects/backend/spring-oktyabr5-project/src/main/java/az/developia/springoktyabr5project/controller/rest/MyFileRestController.java
@@ -1,9 +1,17 @@
 package az.developia.springoktyabr5project.controller.rest;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import az.developia.springoktyabr5project.model.FileResponseModel;
 
@@ -13,9 +21,20 @@ import az.developia.springoktyabr5project.model.FileResponseModel;
 public class MyFileRestController {
 	
 	@PostMapping(path = "/upload")
-	public FileResponseModel uploadFile() {
+	public FileResponseModel uploadFile(@RequestParam(name="file") MultipartFile file) {
 		FileResponseModel model = new FileResponseModel();
-		model.setImageName("t5r4e-3ewe4e-3ewe4t-5tre3e.png");
+		try {
+			String fileName = file.getOriginalFilename();
+			String fileFormat = fileName.substring(fileName.lastIndexOf("."));
+			String randomName = UUID.randomUUID().toString()+fileFormat;
+			model.setImageName(randomName);
+			Files.copy(file.getInputStream(), Paths.get("C:\\Users\\User\\Desktop\\project-files\\"+randomName),
+					StandardCopyOption.REPLACE_EXISTING);
+		} catch (Exception e) {
+			
+		}
+		
+		
 		return model;
 	}
 }
