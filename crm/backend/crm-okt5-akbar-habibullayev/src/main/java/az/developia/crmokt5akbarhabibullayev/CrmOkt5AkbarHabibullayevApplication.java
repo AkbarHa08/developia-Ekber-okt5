55ee2547cb6520c33e0a407f001de70aac105fc0
AkbarHa08 <68279197+AkbarHa08@users.noreply.github.com>
2022-02-13 16:52:27 +0300

    back crm arxivden cixarildi

---
+++ crm/backend/crm-okt5-akbar-habibullayev/src/main/java/az/developia/crmokt5akbarhabibullayev/CrmOkt5AkbarHabibullayevApplication.java
@@ -0,0 +1,13 @@
+package az.developia.crmokt5akbarhabibullayev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmOkt5AkbarHabibullayevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmOkt5AkbarHabibullayevApplication.class, args);
+	}
+
+}
---
+++ crm/backend/crm-okt5-akbar-habibullayev/src/test/java/az/developia/crmokt5akbarhabibullayev/CrmOkt5AkbarHabibullayevApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.crmokt5akbarhabibullayev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmOkt5AkbarHabibullayevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
