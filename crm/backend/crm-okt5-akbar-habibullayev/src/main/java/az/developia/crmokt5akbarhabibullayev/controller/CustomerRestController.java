751edee63c9c0f9bb2017843b9265845e0e90d66
AkbarHa08 <68279197+AkbarHa08@users.noreply.github.com>
2022-02-20 16:42:50 +0300

    return all customers

--- crm/backend/crm-okt5-akbar-habibullayev/src/main/java/az/developia/crmokt5akbarhabibullayev/controller/CustomerRestController.java
+++ crm/backend/crm-okt5-akbar-habibullayev/src/main/java/az/developia/crmokt5akbarhabibullayev/controller/CustomerRestController.java
@@ -1,6 +1,7 @@
 package az.developia.crmokt5akbarhabibullayev.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.validation.Valid;
 import javax.validation.ValidationException;
@@ -11,6 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,6 +52,12 @@ public class CustomerRestController {
 		}
 		
 		return errors;
+	}
+	
+	@GetMapping
+	public List<Customer> getAllCustomers(){
+		List<Customer> customers = customerRepository.findAll(); 
+		return customers;
 	}
 	
 }
