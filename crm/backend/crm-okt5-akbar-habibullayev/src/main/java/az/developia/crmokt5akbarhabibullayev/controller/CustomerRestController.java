468add404033575b55528592cdc461710ddb03c6
AkbarHa08 <68279197+AkbarHa08@users.noreply.github.com>
2022-02-20 17:16:09 +0300

    delete customer by id

--- crm/backend/crm-okt5-akbar-habibullayev/src/main/java/az/developia/crmokt5akbarhabibullayev/controller/CustomerRestController.java
+++ crm/backend/crm-okt5-akbar-habibullayev/src/main/java/az/developia/crmokt5akbarhabibullayev/controller/CustomerRestController.java
@@ -6,13 +6,17 @@ import java.util.List;
 import javax.validation.Valid;
 import javax.validation.ValidationException;
 
+import org.hibernate.annotations.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -58,6 +62,24 @@ public class CustomerRestController {
 	public List<Customer> getAllCustomers(){
 		List<Customer> customers = customerRepository.findAll(); 
 		return customers;
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public void deleteById(@PathVariable(name = "id") Integer id) {
+		boolean customerExists = customerRepository.findById(id).isPresent();
+		if(customerExists) {
+			customerRepository.deleteById(id);
+		} else {
+			throw new RuntimeException("Musteri movcud deyil! ID="+id);
+		}
+		
+		
+	}
+	
+	@ExceptionHandler
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	public String handleNotFoundException(NotFoundException exception) {
+		return exception.getMessage();
 	}
 	
 }
