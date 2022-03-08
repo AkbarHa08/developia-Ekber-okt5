259cb84b80ea49d918e12cfed64d65eb13124c4a
AkbarHa08 <68279197+AkbarHa08@users.noreply.github.com>
2022-03-08 16:40:32 +0300

    Revert "User ve Authority model sinifleri ve repositoryler yaradildi"
    This reverts commit 3fb29f97c1c57a23075a76b0aab8ff68d30c7bb2.

--- last-project/backend/computer-shopping-ekber-hebibullayev/src/main/java/az/developia/computershoppingekberhebibullayev/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package az.developia.computershoppingekberhebibullayev.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import az.developia.computershoppingekberhebibullayev.model.Authority;
-
-public interface AuthorityRepository extends JpaRepository<Authority, Integer>{
-
-}
--- last-project/backend/computer-shopping-ekber-hebibullayev/src/main/java/az/developia/computershoppingekberhebibullayev/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package az.developia.computershoppingekberhebibullayev.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import az.developia.computershoppingekberhebibullayev.model.User;
-
-public interface UserRepository extends JpaRepository<User, String>{
-
-}
