2d48592bb0c381cfa6eade4160695b8272829e47
AkbarHa08 <68279197+AkbarHa08@users.noreply.github.com>
2022-03-08 14:55:28 +0300

    file zipden cixarildi

---
+++ last-project/backend/computer-shopping-ekber-hebibullayev/src/main/java/az/developia/computershoppingekberhebibullayev/ComputerShoppingEkberHebibullayevApplication.java
@@ -0,0 +1,13 @@
+package az.developia.computershoppingekberhebibullayev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerShoppingEkberHebibullayevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerShoppingEkberHebibullayevApplication.class, args);
+	}
+
+}
---
+++ last-project/backend/computer-shopping-ekber-hebibullayev/src/test/java/az/developia/computershoppingekberhebibullayev/ComputerShoppingEkberHebibullayevApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.computershoppingekberhebibullayev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerShoppingEkberHebibullayevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
