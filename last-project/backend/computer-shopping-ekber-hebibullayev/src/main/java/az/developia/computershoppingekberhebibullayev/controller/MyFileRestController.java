a4aeed8a2f098448bf0bc4d134514ff10ff18c8a
AkbarHa08 <68279197+AkbarHa08@users.noreply.github.com>
2022-03-12 20:28:22 +0300

    fayl upload back end yazildi

---
+++ last-project/backend/computer-shopping-ekber-hebibullayev/src/main/java/az/developia/computershoppingekberhebibullayev/controller/MyFileRestController.java
@@ -0,0 +1,52 @@
+package az.developia.computershoppingekberhebibullayev.controller;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import az.developia.computershoppingekberhebibullayev.model.FileResponseModel;
+
+@RestController
+@RequestMapping(path = "/files")
+@CrossOrigin(origins = "*")
+public class MyFileRestController {
+
+	@PostMapping(path = "/upload")
+	public FileResponseModel uploadFile(@RequestParam(name = "file") MultipartFile file) {
+		FileResponseModel model = new FileResponseModel();
+		try {
+			String fileName = file.getOriginalFilename();
+			String fileFormat = fileName.substring(fileName.lastIndexOf("."));
+			String randomName = UUID.randomUUID().toString()+fileFormat;
+			model.setImageName(randomName);
+			
+			File uploadFolder = new File("C:\\Users\\User\\Documents\\GitHub\\developia-Ekber-okt5\\last-project\\images\\");
+			if (uploadFolder.exists()) {
+				System.out.println("Folder movcuddur!");
+			} else {
+				System.out.println("Folder yarandi");
+				uploadFolder.mkdir();
+			}
+			Files.copy(file.getInputStream(), Paths.get("C:\\Users\\User\\Documents\\GitHub\\developia-Ekber-okt5\\last-project\\images\\"+randomName),
+					StandardCopyOption.REPLACE_EXISTING);
+			
+		} catch (Exception e) {
+			
+		}
+		
+		return model;
+		
+		
+		
+		
+	}
+}
---
+++ last-project/backend/computer-shopping-ekber-hebibullayev/src/main/java/az/developia/computershoppingekberhebibullayev/model/FileResponseModel.java
@@ -0,0 +1,16 @@
+package az.developia.computershoppingekberhebibullayev.model;
+
+public class FileResponseModel {
+
+	private String imageName;
+
+	public String getImageName() {
+		return imageName;
+	}
+
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+	
+	
+}
